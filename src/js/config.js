310df1e3db54a03703d21a6220fa15b6bd9f9b62
Eras242 <sol@erascollective.com>
2022-08-20 00:14:39 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "27a523d6-fd09-45ce-862b-45f35b5c9ea8"
+
+"
